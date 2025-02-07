a378c76ab9d86359cd4886689adc8c47bf4a8b06
sohuiyang <140875875+abovavg@users.noreply.github.com>
2025-02-07 10:37:12 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -0,0 +1,11 @@
+def solution(s1, s2):
+    answer = 0
+    if s1 > s2:
+        for i in s2:
+            if i in s1:
+                answer += 1
+    else:
+        for i in s1:
+            if i in s2:
+                answer +=1
+    return answer
