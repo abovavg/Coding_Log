fe67e8c3900cfd4bfee38c48c840d230f32de259
sohuiyang <140875875+abovavg@users.noreply.github.com>
2025-02-14 10:12:15 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    for i in my_string:
+        if i.isnumeric() == True:
+            answer.append(int(i))
+        
+    answer = sorted(answer)
+    return answer
